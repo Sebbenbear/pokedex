9dbe881ac7da2c5078c1981afafc9b7be7146f2c
pauline <roses-chick@hotmail.com>
2014-01-22 16:53:52 +1300

    initial commit. pokemon.txt and main class added.

---
+++ src/UI/pokedex.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class pokedex {
+	
+	//fields
+	
+	
+	
+	public static void main(String[] args) {
+		//new Pokedex();
+		
+		System.out.println("Welcome to Pauline's Pokedex.");
+		
+		
+	}
+
+}
